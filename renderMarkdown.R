e167e173bb97f8637ecf2f19aa293b782f12f839
Paul Schmidt <paul.schmidt@uni-hohenheim.de>
2018-10-04 09:33:45 +0200

    clean slate

--- renderMarkdown.R
+++
@@ -1,3 +0,0 @@
-setwd("D:/RKurse/Dokumentation/crashcouRse")
-library("rmarkdown")
-rmarkdown::render_site
---
+++ render_site.R
@@ -0,0 +1,4 @@
+setwd("D:/RKurse/Dokumentation/crashcouRse")
+library("rmarkdown")
+
+rmarkdown::render_site()
